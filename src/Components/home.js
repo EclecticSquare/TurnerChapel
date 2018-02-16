fe98b6f59a0b42a547cef6b1f45c4c7d37740302
Tina Bryson <tinabryson@Tinas-MacBook-Pro.local>
2018-02-16 12:40:30 -0500

    added border and service hours

--- src/Components/home.js
+++ src/Components/home.js
@@ -11,10 +11,16 @@ class Home extends Component {
                         <div className="row">
                             <div className="col">
                                 <img className='logo' alt='church logo'src={Logo} />
-                                <h4>4650 Cascade Road, Atlanta, Georgia 30331</h4>
+                                <h4 className='address'>4650 Cascade Road, Atlanta, Georgia 30331</h4>
+                                <h5>SUNDAY MORNING WORSHIP SERVICES</h5>
+                                <h6>8am and 11am</h6>
+                                <h5>WEDNESDAY BIBLE STUDY</h5>
+                                <h6>12:30pm and 7:00pm</h6>
                             </div>
-                            <div className="col-8">Column Two
-                            
+                            <div className="col-8">
+                                <h1 className="welcome">Welcome</h1>
+                                <h1 className='mission'>Greater Turner Chapel African Methodist Episcopal Church seeks to be a people that abide in God's Love for ALL people. Empowering and Encouraging one another - Exalting God.</h1>
+                                
                             </div>
                         </div>
                     </div>
