c3365aaf90ed30f2a683cc14804ad02d84d7b344
Tina Bryson <tinabryson@tinas-mbp.attlocal.net>
2018-02-20 22:59:17 -0500

    updated social icons

--- src/Components/home.js
+++ src/Components/home.js
@@ -4,6 +4,8 @@ import {MdHome, MdLocationCity, MdPerson, MdFlare, MdPhoneInTalk} from 'react-ic
 import FaIconPack from 'react-icons/lib/fa'
 import FaCalendar from 'react-icons/lib/fa/calendar'
 import FaGift from 'react-icons/lib/fa/gift'
+import FaTwitter from 'react-icons/lib/fa/twitter'
+import FaFacebook from 'react-icons/lib/fa/facebook'
 import Logo from '../Images/turner_logo.jpg'
 import './home.css';
 import Door from '../Images/openDoor.jpg';
@@ -41,7 +43,7 @@ class Home extends Component {
                                         <a className="nav-link" href="#"><span className='calendarIcon'><FaCalendar/></span>NEWS AND EVENTS</a>
                                     </li>
                                     <li className="nav-item">
-                                        <a className="nav-link" href="#"><span className='giftIcon'><FaGift/></span>TITHE ONLINE</a>
+                                        <a className="nav-link" href="#"><span className='giftIcon'><FaGift/></span>TITHES AND OFFERINGS</a>
                                     </li>
                                     <li className="nav-item">
                                         <a className="nav-link" href="#"><span className='ministryIcon'><MdFlare/></span>MINISTRIES</a>
@@ -62,7 +64,10 @@ class Home extends Component {
                                 <img className='door' alt='open door'src={Door} />
                                 <h1 className="welcome">Welcome</h1>
                                 <h1 className='mission'>Greater Turner Chapel African Methodist Episcopal Church seeks to be a people that abide in God's Love for ALL people. Empowering and Encouraging one another - Exalting God.</h1>
-                                
+                                <div className='follow'>
+                                    <h1 className='followText'>Follow Us</h1>
+                                    <div className='followImages'> <span><FaTwitter/></span>  <span><FaFacebook/></span>   </div>
+                                </div>
                             </div>
                         </div>
                     </div>
