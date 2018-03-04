468514f797e752d8120117e29111d2042b8bc730
Tina Bryson <twalnbryson@gmail.com>
2018-03-03 22:15:42 -0500

    updated pastor component

--- src/Components/pastor.js
+++ src/Components/pastor.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import '../css/pastor.css'
 import Logo from '../Images/turner_logo.jpg'
+import pastorImage from '../Images/pastor.jpg'
 
 
 class Pastor extends Component {
@@ -22,9 +23,16 @@ class Pastor extends Component {
                         </div>
                     </div>
                 </nav>
-
-                <div className='introImage'></div>
-
+                
+                <div className="container-fluid">
+                <div className="row">
+                    <div className='holder'>
+                        <img className='pastorImage' src={pastorImage}/>
+                        <h1 className='display-3 pastorTitle'>Our Pastor</h1>
+                            <p className='paraText mb-0'>Maecenas orci ipsum, sagittis at elit sed, pellentesque ultricies erat. Ut blandit eleifend elit quis elementum. Suspendisse id laoreet tellus. Maecenas enim leo, aliquet nec diam non, posuere pulvinar felis. Integer et diam non dui facilisis finibus nec sit amet tellus. Sed convallis risus nec tortor eleifend lacinia. Phasellus tempor congue dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis vehicula urna, at suscipit nibh. Aenean faucibus, ante nec semper tempus, libero mi tristique augue, ut vulputate elit nibh a est. Nullam quis efficitur mi.<br/> Maecenas enim leo, aliquet nec diam non, posuere pulvinar felis. Integer et diam non dui facilisis finibus nec sit amet tellus. Sed convallis risus nec tortor eleifend lacinia. Phasellus tempor congue dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>Ut blandit eleifend elit quis elementum. Suspendisse id laoreet tellus. Maecenas enim leo, aliquet nec diam non, posuere pulvinar felis. Integer et diam non dui facilisis finibus nec sit amet tellus. Sed convallis risus nec tortor eleifend lacinia. Phasellus tempor congue dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis vehicula urna, at suscipit nibh. Aenean faucibus, ante nec semper tempus, libero mi tristique augue, ut vulputate elit nibh a est. Nullam quis efficitur mi.</p>
+                    </div>
+                </div>
+            </div>
             </div>
         )
     }
