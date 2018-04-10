fc39cebfe0470dba20f9cdd09d5937dfaa78fade
Tina Bryson <twalnbryson@gmail.com>
2018-04-10 14:00:57 -0400

    still working on slideshow

--- src/Components/slideshow.js
+++ src/Components/slideshow.js
@@ -6,15 +6,18 @@ import Image3 from '../Images/3.jpg'
 import Image4 from '../Images/4.jpg'
 import Image5 from '../Images/5.jpg'
 import Image6 from '../Images/1.jpg'
+import Image7 from '../Images/turner_logo.jpg'
 //this is a component as a function
 const slideshow = (props) => {
     return (
         <div className=''>
             <div id='slideshowContainer'>
                 <div id='slideshow'>
+                    <img  className='mainImage' src={Image7}/>
                     <img className='mainImage' src={Image1}/>
                     <img  className='mainImage' src={Image5}/>
                     <img  className='mainImage' src={Image3}/>
+                    <img  className='mainImage' src={Image6}/>
                 </div>
             </div>
             
