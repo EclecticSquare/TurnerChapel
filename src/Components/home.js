cbf476477eb933ac91e8492cb16896bcbd6bd1ac
Tina Bryson <twalnbryson@gmail.com>
2018-04-19 11:49:49 -0400

    updated landing page

--- src/Components/home.js
+++ src/Components/home.js
@@ -12,33 +12,57 @@ import '../css/home.css';
 import Landing from '../Components/landing'
 // import Menu from '../Components/menu'
 import Slideshow from '../Components/slideshow'
+import ImageA from '../Images/turner_logo.jpg'
 
 class Home extends Component {
     render() {
         return(
             <div className=''>
                 {/* navbar */}
-
+                <ul className="nav justify-content-end fixed-top">
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#">Active</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
               
                 <div className='container-fluid'>
                     <div className='row'>
 
                         {/* slideshow */}
-                        <div className='col-11'>
+                        <div className='col-10'>
                             <Landing/>
                         </div>
 
                         {/* information buttons */}
-                        <div className='col-1'>
-
+                        <div className='col-2'>
+                            <br/>
+                            <br/>
+                            
+                            <img src={ImageA}/>
+                            <div className="btn-group-vertical btn-group-lg">
+                                <button type="button" className="btn btn-secondary">Our Story</button>
+                                <button type="button" className="btn btn-secondary">Meet the Pastor</button>
+                                <button type="button" className="btn btn-secondary">Tithes and Offerings</button>
+                                <button type="button" class="btn btn-secondary">Right</button>
+                                <button type="button" class="btn btn-secondary">Right</button>
+                                <button type="button" class="btn btn-secondary">Right</button>
+                            </div>
                         </div>
                         
                     </div>
                 </div>
 
 
-
-                {/* information buttons */}
+                
                 {/* follow me */}
             </div>
 
