bceedff5ac5c2d78c3071b9f691b06663bdff345
Tina Bryson <twalnbryson@gmail.com>
2018-04-22 13:37:30 -0400

    updating pastor page

--- src/Components/pastor.js
+++ src/Components/pastor.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../css/pastor.css'
 import Logo from '../Images/turner_logo.jpg'
 import {Link} from 'react-router-dom';
-// import pastorImage from '../Images/pastor.jpg'
+import pastorImage from '../Images/pastorStandin.jpeg'
 
 
 class Pastor extends Component {
@@ -33,16 +33,18 @@ class Pastor extends Component {
                         <Link to='/contact'><a className="pastor nav-link" href="#">Contact Us</a></Link>
                     </li>
                 </ul>
+
+                
                 
                 {/* pastor Info */}
                 <div className='pastorContainer'>
                     <h1 className='display-3 pastorTitle'>Toni Belin Ingram</h1>
-                    {/* <img className='pastorImage' src={pastorImage}/> */}
-                    <p className='pastorInfo'>Maecenas orci ipsum, sagittis at elit sed, pellentesque ultricies erat. Ut blandit eleifend elit quis elementum. Suspendisse id laoreet tellus. Maecenas enim leo, aliquet nec diam non, posuere pulvinar felis. Integer et diam non dui facilisis finibus nec sit amet tellus. Sed convallis risus nec tortor eleifend lacinia. Phasellus tempor congue dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis vehicula urna, at suscipit nibh. Aenean faucibus, ante nec semper tempus, libero mi tristique augue, ut vulputate elit nibh a est. Nullam quis efficitur mi.<br/> Maecenas enim leo, aliquet nec diam non, posuere pulvinar felis. Integer et diam non dui facilisis finibus nec sit amet tellus. Sed convallis risus nec tortor eleifend lacinia. Phasellus tempor congue dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>Ut blandit eleifend elit quis elementum. Suspendisse id laoreet tellus. Maecenas enim leo, aliquet nec diam non, posuere pulvinar felis. Integer et diam non dui facilisis finibus nec sit amet tellus. Sed convallis risus nec tortor eleifend lacinia. Phasellus tempor congue dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis vehicula urna, at suscipit nibh. Aenean faucibus, ante nec semper tempus, libero mi tristique augue, ut vulputate elit nibh a est. Nullam quis efficitur mi.</p>
+                   
+                    <p className='pastorInfo'><img className='pastorImage' src={pastorImage}/>Maecenas orci ipsum, sagittis at elit sed, pellentesque ultricies erat. Ut blandit eleifend elit quis elementum. Suspendisse id laoreet tellus. Maecenas enim leo, aliquet nec diam non, posuere pulvinar felis. Integer et diam non dui facilisis finibus nec sit amet tellus.  Sed convallis risus nec tortor eleifend lacinia. Phasellus tempor congue dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis vehicula urna, at suscipit nibh. Aenean faucibus, ante nec semper tempus, libero mi tristique augue, ut vulputate elit nibh a est. Nullam quis efficitur mi.<br/> Maecenas enim leo, aliquet nec diam non, posuere pulvinar felis. Integer et diam non dui facilisis finibus nec sit amet tellus. Sed convallis risus nec tortor eleifend lacinia. Phasellus tempor congue dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br/>Ut blandit eleifend elit quis elementum. Suspendisse id laoreet tellus. Maecenas enim leo, aliquet nec diam non, posuere pulvinar felis. Integer et diam non dui facilisis finibus nec sit amet tellus. Sed convallis risus nec tortor eleifend lacinia. Phasellus tempor congue dignissim. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis vehicula urna, at suscipit nibh. Aenean faucibus, ante nec semper tempus, libero mi tristique augue, ut vulputate elit nibh a est. Nullam quis efficitur mi.</p>
                 </div>
 
 
-
+                {/* randomized images */}
 
 
             </div>
