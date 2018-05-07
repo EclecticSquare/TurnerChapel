59518f4d7698b1db689d91ee8ad9b5560879663f
Tina Bryson <twalnbryson@gmail.com>
2018-05-07 18:26:14 -0400

    updated contact page

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -41,13 +41,23 @@ class Contact extends Component {
                     <Link to='/home'><img className="cornerLogo" src={Logo}/></Link>
                 </div>
 
-                <h1 className='display-4 contactTitle'>Contact Us</h1>
-                <h4 className='contactTag'>If the Spirit moves you, reach out!</h4>
+                <div className='container-fluid'>
+                    <div className='row'>
+                            <div className='col-8'>
+                                <h1 className='display-4 contactTitle'>Contact Us</h1>
+                                <h4 className='contactTag'>If the Spirit moves you, reach out!</h4>
+                                <iframe className='form' src="https://docs.google.com/forms/d/e/1FAIpQLSeQb-gmaiVjussd8CY8B8aC9Xq2mDDyHN29DEXtXv7UCL-bIA/viewform?embedded=true" width="100%" height="85%" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>                            </div>
 
-
-
-
-
+                            <div className='col-4'>
+                                <p className='address'> 
+                                    Greater Turner Chapel A.M.E <br/>
+                                    4650 Cascade Rd. <br/>
+                                    Atlanta, GA 30331 <br/>
+                                    404.699.0023
+                                </p>
+                            </div>
+                    </div>
+                </div>
 
             </div>
         )
